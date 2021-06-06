794386238fabcf1d8cffe71efb9f67951cdb9054
Binh <thanhbinh710@gmail.com>
2021-06-06 21:36:59 +0800

    add tell stitcher app method

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,22 @@
   <div class="container">
     <div class="row justify-content-center align-item-center text-center">
       <div class="col-12 col-md-10 col-lg-8">
-        <h3>This child application aims to show you how communication between can be done</h3>
+        <h3>Child app 1</h3>
+        <p>This child application aims to show you how communication between stitcher app and parent can be done</p>
+        <br/>
+        <br/>
+        
+        <div class="form-group">
+          <label for="messageToParent">Enter something you want to tell stitcher app</label>
+          <input type="text" class="form-control centered-text" id="messageToParent" v-model="message">
+        </div>
+        
+        <br/>
+        <br/>
+
+        <button class="btn btn-primary bold-text" @click="tellStitcherApp()">
+          Tell stitcher app
+        </button>
       </div>
     </div>
   </div>
@@ -10,8 +25,27 @@
 </template>
 
 <script>
+import { updateParentApp } from '@/helpers/updateParentRoute'
+
 export default {
   name: 'App',
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    tellStitcherApp() {
+      // create the message to inform stitcher app
+      const msg = {
+        action: 'alert',
+        info: this.message
+      }
+      
+      // call global helper method to update stitcher app about the route change
+      updateParentApp(msg)
+    }
+  }
 }
 </script>
 
@@ -25,8 +59,14 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
   background-color: aliceblue;
-
+  height: 100vh;
+}
+label {
+  margin-bottom: 8px;
+}
+.centered-text {
+  text-align: center;
 }
 </style>
---
+++ src/helpers/updateParentRoute.js
@@ -0,0 +1,12 @@
+function updateParentApp(msg) {
+  const stitcherAppDomain = process.env.VUE_APP_STITCHER_DOMAIN // get stitcher app URL
+
+  try {
+    // postmessage to stitcher app
+    window.parent.postMessage(msg, stitcherAppDomain);
+  } catch(e) {
+    console.error('error posting message to stitcher app', e);
+  }
+}
+
+export { updateParentApp };
